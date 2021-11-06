e4df76de4d379361614c9367b9f0fed2f90ba6c2
Anthonin Clara <anthonin.clara@epitech.eu>
2021-11-06 13:52:34 +0100

    feat: add the theme to the app

--- movies/lib/src/app.dart
+++ movies/lib/src/app.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:movies/src/theme/app_colors.dart';
+import 'package:movies/src/theme/app_theme.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: AppColors.background,
+      statusBarBrightness: Brightness.dark
+    ));
     return MaterialApp(
       title: 'Movies',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: AppTheme.defaultTheme,
       home: const HomePage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -28,8 +32,12 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          child: const Text("Main"),
+        child: ElevatedButton(
+          onPressed: null,
+          child: const Text("Hello"),
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(
+                  Theme.of(context).primaryColor)),
         ),
       ),
     );
