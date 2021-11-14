c4553b299544bc164f20742da429d13ee79c0dcb
Anthonin Clara <anthonin.clara@epitech.eu>
2021-11-14 19:51:40 +0100

    fix: change the provider url to the constant in the configuration file

--- movies/lib/resources/api_provider.dart
+++ movies/lib/resources/api_provider.dart
@@ -1,9 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:movies/models/covid_model.dart';
+import 'package:movies/config/constants.dart' as Constants;
 
 class ApiProvider {
   final Dio _dio = Dio();
-  final String _url = 'http://localhost:8080/';
+  final String _url = '${Constants.API_URL}:8080/';
 
   Future<CovidModel> fetchCovidList() async {
     try {
