2e2fa52ed359803fe8e79ad72e4dd1e561bdac11
Anthonin Clara <anthonin.clara@epitech.eu>
2021-11-14 16:40:03 +0100

    fix: overflow in profile when typing in the email field

--- movies/lib/src/pages/profile_page.dart
+++ movies/lib/src/pages/profile_page.dart
@@ -38,184 +38,187 @@ class _ProfilePage extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/profile_screen.png"),
-          fit: BoxFit.cover,
+    return SingleChildScrollView(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/profile_screen.png"),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      child: StreamBuilder<User>(
-        stream: userBloc.user,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            _controller.text = snapshot.data!.email;
-            return Column(children: [
-              Container(
-                margin: const EdgeInsets.only(top: 50, left: 330),
-                child: IconButton(
-                  onPressed: () {
-                    print("Button pen");
-                  },
-                  icon: const Icon(
-                    MdiIcons.pencil,
-                    size: 24,
-                    color: Colors.white,
+        child: StreamBuilder<User>(
+          stream: userBloc.user,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              _controller.text = snapshot.data!.email;
+              return Column(children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 50, left: 330),
+                  child: IconButton(
+                    onPressed: () {
+                      print("Button pen");
+                    },
+                    icon: const Icon(
+                      MdiIcons.pencil,
+                      size: 24,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                margin: const EdgeInsets.only(top: 25),
-                height: 617,
-                child: Stack(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(
-                        top: 40,
-                        left: 10,
-                        right: 10,
-                      ),
-                      height: MediaQuery.of(context).size.height,
-                      width: MediaQuery.of(context).size.width,
-                      decoration: const BoxDecoration(
-                        color: Color(0xFFF5F8FF),
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(50),
-                          topRight: Radius.circular(50),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.only(top: 25),
+                  height: 617,
+                  child: Stack(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(
+                          top: 40,
+                          left: 10,
+                          right: 10,
+                        ),
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: const BoxDecoration(
+                          color: Color(0xFFF5F8FF),
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(50),
+                            topRight: Radius.circular(50),
+                          ),
                         ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment.topCenter,
-                      child: Container(
-                        margin: const EdgeInsets.only(bottom: 0),
+                      Align(
+                        alignment: Alignment.topCenter,
                         child: Container(
-                          width: 100,
-                          height: 100,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Theme.of(context).primaryColorDark,
-                          ),
-                          child: Center(
-                            child: Text(
-                              snapshot.data!.username.isNotEmpty
-                                  ? snapshot.data!.username[0].toUpperCase()
-                                  : "?",
-                              style: const TextStyle(
-                                fontSize: 50,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
+                          margin: const EdgeInsets.only(bottom: 0),
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Theme.of(context).primaryColorDark,
+                            ),
+                            child: Center(
+                              child: Text(
+                                snapshot.data!.username.isNotEmpty
+                                    ? snapshot.data!.username[0].toUpperCase()
+                                    : "?",
+                                style: const TextStyle(
+                                  fontSize: 50,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment.topCenter,
-                      child: Container(
-                        margin: const EdgeInsets.only(top: 65, left: 75),
-                        child: Image.asset('assets/images/Flag.png'),
+                      Align(
+                        alignment: Alignment.topCenter,
+                        child: Container(
+                          margin: const EdgeInsets.only(top: 65, left: 75),
+                          child: Image.asset('assets/images/Flag.png'),
+                        ),
                       ),
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.only(top: 120),
-                          child: Column(
-                            children: [
-                              Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Text(
-                                  snapshot.data!.username,
-                                  style: const TextStyle(
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                              const ProfileInformation(),
-                              Container(
-                                margin: const EdgeInsets.only(
-                                  top: 20,
-                                  right: 220,
-                                ),
-                                child: const Text(
-                                  "Email address",
-                                  style: TextStyle(
-                                      color: Color(0xFF9E9E9E), fontSize: 16),
-                                ),
-                              ),
-                              Container(
-                                margin: const EdgeInsets.only(
-                                    top: 10, left: 30, right: 30),
-                                child: TextFormField(
-                                  autocorrect: false,
-                                  cursorColor: Theme.of(context).primaryColor,
-                                  controller: _controller,
-                                  onFieldSubmitted: (value) {
-                                    //Todo: Add Real JWT from localstorage
-                                    updateUser(
-                                        "731b1b6a-7900-4215-8e86-ec3203b72ce4",
-                                        value);
-                                  },
-                                  decoration: InputDecoration(
-                                    focusedBorder: InputBorder.none,
-                                    enabledBorder: OutlineInputBorder(
-                                      borderSide: const BorderSide(
-                                          color: Color(0x409E9E9E), width: 0.0),
-                                      borderRadius: BorderRadius.circular(5.0),
+                      Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(top: 120),
+                            child: Column(
+                              children: [
+                                Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: Text(
+                                    snapshot.data!.username,
+                                    style: const TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
                                     ),
-                                    filled: true,
-                                    fillColor: const Color(0x409E9E9E),
                                   ),
                                 ),
-                              ),
-                              Container(
-                                margin: const EdgeInsets.only(top: 15),
-                                child: TextButton(
-                                  onPressed: () {
-                                    //Todo: Add Real JWT from localstorage
-                                    deleteUser(
-                                        "b134c06e-d554-489a-b77d-30430457e44b");
-                                  },
-                                  style: TextButton.styleFrom(
-                                    primary: Colors.red,
+                                const ProfileInformation(),
+                                Container(
+                                  margin: const EdgeInsets.only(
+                                    top: 20,
+                                    right: 220,
                                   ),
                                   child: const Text(
-                                    "Delete my account",
-                                    style: TextStyle(fontSize: 16),
+                                    "Email address",
+                                    style: TextStyle(
+                                        color: Color(0xFF9E9E9E), fontSize: 16),
                                   ),
                                 ),
-                              ),
-                              Container(
-                                margin: const EdgeInsets.only(top: 65),
-                                child: const Text(
-                                  "COMING SOON",
-                                  style: TextStyle(
-                                    fontSize: 24,
-                                    color: Color(0xFFCED5E3),
-                                    fontWeight: FontWeight.bold,
+                                Container(
+                                  margin: const EdgeInsets.only(
+                                      top: 10, left: 30, right: 30),
+                                  child: TextFormField(
+                                    autocorrect: false,
+                                    cursorColor: Theme.of(context).primaryColor,
+                                    controller: _controller,
+                                    onFieldSubmitted: (value) {
+                                      //Todo: Add Real JWT from localstorage
+                                      updateUser(
+                                          "731b1b6a-7900-4215-8e86-ec3203b72ce4",
+                                          value);
+                                    },
+                                    decoration: InputDecoration(
+                                      focusedBorder: InputBorder.none,
+                                      enabledBorder: OutlineInputBorder(
+                                        borderSide: const BorderSide(
+                                            color: Color(0x409E9E9E), width: 0.0),
+                                        borderRadius: BorderRadius.circular(5.0),
+                                      ),
+                                      filled: true,
+                                      fillColor: const Color(0x409E9E9E),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
+                                Container(
+                                  margin: const EdgeInsets.only(top: 15),
+                                  child: TextButton(
+                                    onPressed: () {
+                                      //Todo: Add Real JWT from localstorage
+                                      deleteUser(
+                                          "b134c06e-d554-489a-b77d-30430457e44b");
+                                    },
+                                    style: TextButton.styleFrom(
+                                      primary: Colors.red,
+                                    ),
+                                    child: const Text(
+                                      "Delete my account",
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(top: 65),
+                                  child: const Text(
+                                    "COMING SOON",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      color: Color(0xFFCED5E3),
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ]);
-          }
-          if (snapshot.hasError) {
-            return Center(child: Text(snapshot.error.toString()));
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+              ]);
+            }
+            if (snapshot.hasError) {
+              return Center(child: Text(snapshot.error.toString()));
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
       ),
     );
   }
