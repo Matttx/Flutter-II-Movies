579ec169c6f198700c00c6f329cc206077272675
Anthonin Clara <anthonin.clara@epitech.eu>
2021-11-15 19:18:29 +0100

    refactor: remove useless import

--- movies/test/card_test.dart
+++ movies/test/card_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:movies/src/package/card.dart';
